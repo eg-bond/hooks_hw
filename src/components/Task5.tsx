a1a048a4c515b6f1a39cc7b89fd10fd977c40a49
eg-bond <eg-bond@yandex.ru>
2024-05-13 19:06:46 +0300

    feat: task 5 done

---
+++ src/components/Task5.tsx
@@ -0,0 +1,124 @@
+import { useWindowScroll } from '../hooks/useWindowScroll'
+
+function Task5() {
+  const [scroll, scrollTo] = useWindowScroll()
+
+  return (
+    <div>
+      <p>
+        Scroll position x: {scroll.x}, y: {scroll.y}
+      </p>
+
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          dolorem officiis, impedit voluptate, expedita consectetur maxime
+          laborum ipsum blanditiis quis officia illum repudiandae error
+          cupiditate veritatis voluptatum vero eligendi asperiores? Praesentium
+          pariatur, optio officiis a quia libero incidunt voluptatibus saepe
+          accusamus error eveniet ratione beatae, porro vel explicabo
+          cupiditate, tempora ducimus dicta quo assumenda eum minus quas ad qui!
+          Itaque! Et, commodi animi iste autem porro eveniet est accusantium
+          error quam quod ipsum molestias pariatur facere placeat saepe dolorum
+          modi magnam! Nulla labore saepe dolore perferendis modi odit non
+          quidem! Optio, consequatur possimus vero non quibusdam doloribus
+          itaque, numquam obcaecati placeat omnis ducimus nisi nihil ipsum
+          consequuntur unde perspiciatis ex. Placeat ipsam reprehenderit a ut
+          dicta et doloremque aspernatur excepturi. Dolorum beatae nemo
+          dignissimos earum maiores ratione, doloremque ex amet, nulla et
+          aperiam assumenda perferendis esse dolores sunt magnam. Molestias
+          optio minima harum, qui error fuga omnis dolorum consequuntur quidem.
+          Quaerat quo quos laboriosam, ducimus id dolorem aperiam dicta atque
+          natus porro quasi magni deserunt nobis eius! Quia facere labore
+          laborum ea et. Soluta alias, numquam magnam eos consequuntur nam.
+          Eveniet delectus totam perspiciatis autem id, incidunt rerum pariatur,
+          vero hic, quisquam debitis ex officia sed. Est, exercitationem optio
+          provident inventore error velit quae voluptatum et nostrum nihil,
+          deleniti reiciendis. Eos illo error iste voluptatibus, minima quia
+          dolor dicta, reprehenderit saepe ea laborum impedit nulla quae libero
+          voluptatum repudiandae neque. Porro molestiae eius eaque minus labore!
+          Modi ipsam vero quae! Aspernatur quibusdam iusto atque, voluptatibus
+          expedita, itaque dolores at impedit, doloremque aliquam neque
+          veritatis sequi. Corrupti dolore libero possimus ipsa debitis, nam
+          laudantium, aliquid eaque, dolor maiores temporibus ex doloremque. Ab
+          omnis ad earum, eaque eos et cum soluta quaerat sint error, sed magni
+          culpa nostrum? Tenetur quis, corporis tempore placeat molestiae
+          dolorum quaerat perferendis, harum ab facilis, repellat ducimus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          dolorem officiis, impedit voluptate, expedita consectetur maxime
+          laborum ipsum blanditiis quis officia illum repudiandae error
+          cupiditate veritatis voluptatum vero eligendi asperiores? Praesentium
+          pariatur, optio officiis a quia libero incidunt voluptatibus saepe
+          accusamus error eveniet ratione beatae, porro vel explicabo
+          cupiditate, tempora ducimus dicta quo assumenda eum minus quas ad qui!
+          Itaque! Et, commodi animi iste autem porro eveniet est accusantium
+          error quam quod ipsum molestias pariatur facere placeat saepe dolorum
+          modi magnam! Nulla labore saepe dolore perferendis modi odit non
+          quidem! Optio, consequatur possimus vero non quibusdam doloribus
+          itaque, numquam obcaecati placeat omnis ducimus nisi nihil ipsum
+          consequuntur unde perspiciatis ex. Placeat ipsam reprehenderit a ut
+          dicta et doloremque aspernatur excepturi. Dolorum beatae nemo
+          dignissimos earum maiores ratione, doloremque ex amet, nulla et
+          aperiam assumenda perferendis esse dolores sunt magnam. Molestias
+          optio minima harum, qui error fuga omnis dolorum consequuntur quidem.
+          Quaerat quo quos laboriosam, ducimus id dolorem aperiam dicta atque
+          natus porro quasi magni deserunt nobis eius! Quia facere labore
+          laborum ea et. Soluta alias, numquam magnam eos consequuntur nam.
+          Eveniet delectus totam perspiciatis autem id, incidunt rerum pariatur,
+          vero hic, quisquam debitis ex officia sed. Est, exercitationem optio
+          provident inventore error velit quae voluptatum et nostrum nihil,
+          deleniti reiciendis. Eos illo error iste voluptatibus, minima quia
+          dolor dicta, reprehenderit saepe ea laborum impedit nulla quae libero
+          voluptatum repudiandae neque. Porro molestiae eius eaque minus labore!
+          Modi ipsam vero quae! Aspernatur quibusdam iusto atque, voluptatibus
+          expedita, itaque dolores at impedit, doloremque aliquam neque
+          veritatis sequi. Corrupti dolore libero possimus ipsa debitis, nam
+          laudantium, aliquid eaque, dolor maiores temporibus ex doloremque. Ab
+          omnis ad earum, eaque eos et cum soluta quaerat sint error, sed magni
+          culpa nostrum? Tenetur quis, corporis tempore placeat molestiae
+          dolorum quaerat perferendis, harum ab facilis, repellat ducimus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          dolorem officiis, impedit voluptate, expedita consectetur maxime
+          laborum ipsum blanditiis quis officia illum repudiandae error
+          cupiditate veritatis voluptatum vero eligendi asperiores? Praesentium
+          pariatur, optio officiis a quia libero incidunt voluptatibus saepe
+          accusamus error eveniet ratione beatae, porro vel explicabo
+          cupiditate, tempora ducimus dicta quo assumenda eum minus quas ad qui!
+          Itaque! Et, commodi animi iste autem porro eveniet est accusantium
+          error quam quod ipsum molestias pariatur facere placeat saepe dolorum
+          modi magnam! Nulla labore saepe dolore perferendis modi odit non
+          quidem! Optio, consequatur possimus vero non quibusdam doloribus
+          itaque, numquam obcaecati placeat omnis ducimus nisi nihil ipsum
+          consequuntur unde perspiciatis ex. Placeat ipsam reprehenderit a ut
+          dicta et doloremque aspernatur excepturi. Dolorum beatae nemo
+          dignissimos earum maiores ratione, doloremque ex amet, nulla et
+          aperiam assumenda perferendis esse dolores sunt magnam. Molestias
+          optio minima harum, qui error fuga omnis dolorum consequuntur quidem.
+          Quaerat quo quos laboriosam, ducimus id dolorem aperiam dicta atque
+          natus porro quasi magni deserunt nobis eius! Quia facere labore
+          laborum ea et. Soluta alias, numquam magnam eos consequuntur nam.
+          Eveniet delectus totam perspiciatis autem id, incidunt rerum pariatur,
+          vero hic, quisquam debitis ex officia sed. Est, exercitationem optio
+          provident inventore error velit quae voluptatum et nostrum nihil,
+          deleniti reiciendis. Eos illo error iste voluptatibus, minima quia
+          dolor dicta, reprehenderit saepe ea laborum impedit nulla quae libero
+          voluptatum repudiandae neque. Porro molestiae eius eaque minus labore!
+          Modi ipsam vero quae! Aspernatur quibusdam iusto atque, voluptatibus
+          expedita, itaque dolores at impedit, doloremque aliquam neque
+          veritatis sequi. Corrupti dolore libero possimus ipsa debitis, nam
+          laudantium, aliquid eaque, dolor maiores temporibus ex doloremque. Ab
+          omnis ad earum, eaque eos et cum soluta quaerat sint error, sed magni
+          culpa nostrum? Tenetur quis, corporis tempore placeat molestiae
+          dolorum quaerat perferendis, harum ab facilis, repellat ducimus.
+        </p>
+        <button onClick={() => scrollTo({ y: 0 })}>Scroll to top</button>
+      </div>
+    </div>
+  )
+}
+
+export default Task5
